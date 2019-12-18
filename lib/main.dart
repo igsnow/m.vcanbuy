89c9dbd5c93b76dee8544cf2386d52f81be3eb02
igsnow <zzy716810918@gmail.com>
2019-12-18 14:58:31 +0800

    研究ios启动引导页

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:m_vcanbuy/widget/browser.dart';
+import 'package:fluro/fluro.dart';
+import 'package:m_vcanbuy/pages/splash_page.dart';
+import 'package:m_vcanbuy/routes/routes.dart';
+import 'package:m_vcanbuy/routes/application.dart';
+
 
 void main() => runApp(new MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Vcanbuy App',
-        theme: new ThemeData(
-          primarySwatch: Colors.orange,
-        ),
-        home: new Browser(
-//              url: "https://www.douyu.com",
-//              url: "https://www.bilibili.com",
-          url: "http://m.vcanbuy.com",
-//          title: "百度",
-        ));
-  }
+    final router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
+    return MaterialApp(
+      title: 'Vcanbuy',
+      onGenerateRoute: Application.router.generator,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: SplashPage(),
+    );  }
 }
