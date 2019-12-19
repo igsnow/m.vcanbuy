cb3bcdaa9db67d6dfe6a85d1d55d37ecd771ddbd
igsnow <zzy716810918@gmail.com>
2019-12-19 18:34:24 +0800

    不兼容了，直接换成https等吧

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:m_vcanbuy/pages/splash_page.dart';
 import 'package:m_vcanbuy/route/routes.dart';
-import 'package:m_vcanbuy/widgets/browser.dart';
-import 'package:m_vcanbuy/widgets/webview.dart';
 import 'application.dart';
 
 void main() => runApp(new MyApp());
@@ -11,16 +9,14 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    Router router = Router();
-//    Routes.configureRoutes(router);
-//    Application.router = router;
+    Router router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
 
     return new MaterialApp(
       title: 'Vcanbuy',
-//      onGenerateRoute: Application.router.generator,
-      home: new WebView(
-        url: "http://m.vcanbuy.com",
-      ),
+      onGenerateRoute: Application.router.generator,
+      home: new SplashPage(),
     );
   }
 }
