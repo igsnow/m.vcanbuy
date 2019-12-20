d59dff1db82f86cf54c7317bf08a8f6e09cae922
igsnow <zzy716810918@gmail.com>
2019-12-20 11:11:00 +0800

    android测试没有联网权限

--- lib/route/route_handles.dart
+++ lib/route/route_handles.dart
@@ -6,6 +6,6 @@ import 'package:m_vcanbuy/widgets/browser.dart';
 var homeHandler = new Handler(
     handlerFunc: (BuildContext context, Map<String, List<Object>> params) {
   return new Browser(
-    url: "http://m.vcanbuy.com",
+    url: "https://www.youku.com",
   );
 });
